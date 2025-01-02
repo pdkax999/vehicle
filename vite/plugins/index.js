b447eee478db1fd80f89c54eee5a7b9e48ff1aea
quyue <10676114@qq.com>
2025-01-02 08:40:57 +0800

    config: 撤销前面的配置

--- vite/plugins/index.js
+++ vite/plugins/index.js
@@ -4,12 +4,9 @@ import createAutoImport from './auto-import'
 import createSvgIcon from './svg-icon'
 import createCompression from './compression'
 import createSetupExtend from './setup-extend'
-import copy from 'vite-plugin-copy'
 
 export default function createVitePlugins(viteEnv, isBuild = false) {
-    const vitePlugins = [vue(),  copy([
-        { src: '/netlify.toml', dest: '' }, // 将文件复制到根目录
-      ])]
+    const vitePlugins = [vue()]
     vitePlugins.push(createAutoImport())
 	vitePlugins.push(createSetupExtend())
     vitePlugins.push(createSvgIcon(isBuild))
